cf1f2e0e7e14f0a82a2a00470593b1492f61e314
shloma <70664634+kleinmanshloma@users.noreply.github.com>
2024-03-27 11:28:03 +0100

    bh! starting with the phone line

--- index.js
+++ index.js
@@ -5,43 +5,56 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-const onInboundCall = (request, response) => {
+const onInboundCall = (req, res) => {
   const ncco = [
     {
       action: "talk",
-      text: "Please enter a digit",
+      text: "Please press 1 to hear the current date and time, or press 2 to listen to a song.",
     },
     {
       action: "input",
-      type: ["dtmf"],
-      eventUrl: [`${request.protocol}://${request.get("host")}/webhooks/dtmf`],
+      eventUrl: [`${req.protocol}://${req.get("host")}/webhooks/dtmf`],
     },
   ];
-
-  response.json(ncco);
+  return ncco;
 };
 
-const onInput = (request, response) => {
+const onInput = (req, res) => {
   const dtmf = request.body.dtmf;
-  console.log(dtmf);
+  let actions = [];
+  let ncco = [];
+  switch (req.body.dtmf.digits) {
+    case "1":
+      actions.push({
+        action: "talk",
+        text: `It is ${new Intl.DateTimeFormat(undefined, {
+          dateStyle: "full",
+          timeStyle: "long",
+        }).format(Date.now())}`,
+      });
+      break;
+    case "2":
+      actions.push({
+        action: "stream",
+        streamUrl: [
+          "https://nexmo-community.github.io/ncco-examples/assets/voice_api_audio_streaming.mp3",
+        ],
+      });
+  }
+  ncco = actions.concat(mainMenu(req));
 
-  const ncco = [
-    {
-      action: "talk",
-      text: `You pressed ${dtmf}`,
-    },
-  ];
+  console.log(ncco);
 
-  response.json(ncco);
+  res.json(ncco);
 };
 
 app.get("/webhooks/answer", onInboundCall).post("/webhooks/dtmf", onInput);
 
 app.post("/webhooks/events", (req, res) => {
   console.log(req.body);
-  res.status(200).end();
+  res.sendStatus(204);
 });
 
-app.listen(80, () => {
-  console.log("Server is running on port 80");
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
